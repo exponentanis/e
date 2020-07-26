a1539ec81eec4eb8eda34d9e59b2a0cce604c1dd
exponenta-magazine <68739211+exponenta-magazine@users.noreply.github.com>
2020-07-27 00:58:54 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,8 +17,10 @@ const about = document.querySelector('.about');
 const howto = document.querySelector('.howto');
 const articleList = document.querySelector('.article-list');
 const cardNum = articleList.getElementsByClassName('card').length;
+const newsCards = document.querySelector('.swiper-wrapper');
 const issuesCards = sw1.querySelector('.swiper-wrapper');
 const articlesCards = sw2.querySelector('.swiper-wrapper');
+const ListArticleCards = document.querySelector('.article-list');
 const releaseCard = issuesCards.querySelectorAll('.img-card');
 const articleCard = articlesCards.querySelectorAll('.img-card');
 const articleCCdo = articleList.querySelectorAll('.img-card');
@@ -38,35 +40,103 @@ async function getData(url){
   }
   return await response.json();
 }
-async function forData(data){
+async function forDataIssues(data){
   data.forEach(createIssues);
 }
 function createIssues(issue){
   const {pdfName, imageName, name, likes, dislikes} = issue;
   const card = `
   <section class="swiper-slide card" data-info = "${pdfName}">
-    <img src="preview_img/issues/${imageName}" alt="logo" class="img-card">
-    <div class="info">
-      <div class="rating"> 
-            <img src="img/like.png" alt="like" class="imlink" style="padding-right: 0px;">
-            <div class="info-text" style="cursor: pointer; margin-right: 8px;">${likes}</div>
-      </div>
-      <div class="info-text">${name}</div>
-      <div class="rating">
-          <div class="info-text"style="cursor: pointer; margin-left: 8px;">${dislikes}</div>
-          <img src="img/dislike.png" alt="like" class="imlink"style="padding-right: 0px;">
-      </div>    
+   <div class="card-container">
+        <img src="preview_img/issues/${imageName}" alt="logo" class="img-card">
+        <div class="info">
+          <div class="rating"> 
+                <img src="img/like.png" alt="like" class="img-rating" style="padding-right: 0px;">
+                <div class="info-text numtxt" style="cursor: pointer;">${likes}</div>
+          </div>
+          <div class="info-text">${name}</div>
+          <div class="rating">
+              <div class="info-text numtxt"style="cursor: pointer;">${dislikes}</div>
+              <img src="img/dislike.png" alt="like" class="img-rating"style="padding-right: 0px;">
+          </div>    
+        </div>
     </div>
   </section> 
   `;
   issuesCards.insertAdjacentHTML('beforeend', card);
 }
+async function forDataArticles(data){
+  data.forEach(function callback(value, iter){
+    createArticles(value, iter);
+  }
+    );
+}
+async function createArticles(article, i){
+  const {pdfName, imageName, name, likes, dislikes} = article;
+  const card = `
+  <section class="swiper-slide card forlist" data-info = "${pdfName}">
+    <div class="card-container forcont">
+        <img src="preview_img/articles/${imageName}" alt="logo" class="img-card getimg" >
+        <div class="info">
+          <div class="rating"> 
+                <img src="img/like.png" alt="like" class="img-rating" style="padding-right: 0px;">
+                <div class="info-text numtxt" style="cursor: pointer; ">${likes}</div>
+          </div>
+          <div class="info-text">${name}</div>
+          <div class="rating">
+              <div class="info-text numtxt"style="cursor: pointer;">${dislikes}</div>
+              <img src="img/dislike.png" alt="like" class="img-rating"style="padding-right: 0px;">
+          </div>    
+        </div>
+  </div>
+  </section> 
+  `;
+  async function crlist(){
+  ListArticleCards.insertAdjacentHTML('beforeend', card);
+  const aca = document.querySelectorAll(".forlist");
+  aca.forEach(function(item){
+    item.classList.add("acard");
+    item.classList.remove("swiper-slide");
+  });
+
+  const cont = document.querySelectorAll('.forcont');
+  cont.forEach(function(cont){
+    cont.classList.remove("card-container");
+  });
+    
+  }
+  await crlist();
+  if(i<6){
+  articlesCards.insertAdjacentHTML('beforeend', card);
+  }
+}
+async function forDataNews(data){
+  data.forEach(function callback(value, iter){
+    createNews(value, iter);
+  });
+}
+function createNews(news){
+  const {title, description, image} = news;
+  const card = `
+  <section class="swiper-slide new" style=" 
+  cursor: pointer;
+  background: url(../preview_img/news/${image}) no-repeat;
+  background-size: 100% 100%;
+  border-radius: 50px;
+  ">
+    <h1 class="news-title">${title}<br/></h1>
+    <p class="news-text">${description}</p>
+  </section> 
+  `;
+  newsCards.insertAdjacentHTML('beforeend', card);
+}
 
 
 //fuck yeaaaaaaaaaaaaaaaaaaah!
-function renderPdf(url, canvasContainer, scale){
-        async function renderPage(page){
-            var viewport = page.getViewport(scale);
+function renderPdf(url, canvasContainer){
+        async function renderPage(page, width){
+            console.log(width);
+            var viewport = page.getViewport(width / page.getViewport(1.0).width);
             var canvas = document.createElement('canvas');
             var child = art.appendChild(canvas);
             child.classList.add("page");
@@ -86,10 +156,12 @@ function renderPdf(url, canvasContainer, scale){
       art = canvasContainer.appendChild(div);
       var num = doc._pdfInfo.numPages+1;
       art.classList.add("pdf-articles");
+      var positionInfo = art.getBoundingClientRect();
+      var width =  positionInfo.width;
       async function loop(doc){
         for (var i=1;i<num;i++){
               await doc.getPage(i).then( async function(page){
-              await renderPage(page);
+              await renderPage(page, width);
               });
               const pdfArticle = document.querySelector(".pdf-articles");
               if (!pdfArticle){
@@ -162,7 +234,7 @@ function openRelease(){
   const card = target.closest('.card');
   const info = card.dataset.info.split(',');
   const [pdfName] = info;
-  renderPdf(`./pdfs/issues/${pdfName}`, main, 3);
+  renderPdf(`./pdfs/issues/${pdfName}`, main);
   hideAll();
   menu.classList.add('vertical');
   menu.classList.remove('menu');
@@ -176,7 +248,7 @@ function openArticle(){
   const card = target.closest('.card');
   const info = card.dataset.info.split(',');
   const [pdfName] = info;
-  renderPdf(`./pdfs/articles/${pdfName}`, main, 3);
+  renderPdf(`./pdfs/articles/${pdfName}`, main);
   hideAll();
   menu.classList.add('vertical');
   menu.classList.remove('menu');
@@ -187,35 +259,23 @@ function openArticle(){
   
 }
 
-  if (cardNum %4 !=0){
-    for(var i=0; i<4-cardNum %4; i++){
-      const addcard = document.createElement('div');
-      addcard.className = 'transparent';
-      addcard.insertAdjacentHTML('beforeend', `
-      <div class="acard ">
-
-      </div> 
-      `);
-      articleList.insertAdjacentElement('beforeend', addcard);
-    }
-  }
+ 
 
 function initSwipe(){
 const newsSwiper = new Swiper('.news-swiper',{
   autoplay: {delay: 3000,},
-  spaceBetween: 100,
   sleidesPerColumn: 1,
   disableOnInteraction: false,
 });
 const artSwiper1 = new Swiper('.sw1',{
   autoplay: {delay: 3000,},
-  spaceBetween: 100,
+  spaceBetween: 0,
   slidesPerView: 3,
   disableOnInteraction: true,
 });
 const artSwiper2 = new Swiper('.sw2',{
   autoplay: {delay: 3000,},
-  spaceBetween: 100,
+  spaceBetween: 0,
   slidesPerView: 3,
   disableOnInteraction: true,
 });
@@ -234,11 +294,27 @@ sw2.addEventListener("mouseout",function() {
 }
 
 async function init(){
-  const data = await getData('./db/issues.json');
-  await forData(data);
-  
+  const dataIssues = await getData('./db/issues.json');
+  await forDataIssues(dataIssues);
+  const dataArticles = await getData('./db/articles.json');
+  await forDataArticles(dataArticles);
+  const dataNews = await getData('./db/news.json');
+  await forDataNews(dataNews);
 
 
+
+ if (cardNum %4 !=0){
+    for(var i=0; i<4-cardNum %4; i++){
+      const addcard = document.createElement('div');
+      addcard.className = 'transparent';
+      addcard.insertAdjacentHTML('beforeend', `
+      <div class="acard ">
+
+      </div> 
+      `);
+      articleList.insertAdjacentElement('beforeend', addcard);
+    }
+  }
   const releaseCard = issuesCards.querySelectorAll('.img-card');
   const articleCard = articlesCards.querySelectorAll('.img-card');
   const articleCCdo = articleList.querySelectorAll('.img-card');
