1d2c1a2589ef11f59e9ba028003eede260c1a66a
exponenta <68739211+exponentanis@users.noreply.github.com>
2021-01-10 20:35:27 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -32,8 +32,8 @@ const articleCCdo = articleList.querySelectorAll('.img-card');
 const pdfArticles = document.getElementById("pdf-articles");
 const returnButton = document.querySelector(".return-button");
 const rating = document.querySelectorAll('.rating');
-const podcamul = document.getElementById('podcamul');
-const podca = document.getElementById('.podca');
+const podcamul = document.querySelector('podcamul');
+const podca = document.querySelector('.podca');
 
 
 
@@ -239,8 +239,8 @@ function clearPdf(){
       }
   }
 function hideAll(){
-  const podcamul = document.getElementById('podcamul');
-  const podca = document.getElementById('.podca');
+  const podcamul = document.querySelector('podcamul');
+  const podca = document.querySelector('.podca');
   podca.classList.add('hide');
   news.classList.add('hide');
   releases.classList.add('hide');
@@ -712,8 +712,8 @@ async function init(podcamul,podca){
 }
 
 window.onload = function () {
-  const podcamul = document.getElementById('podcamul');
-  const podca = document.getElementById('.podca');
+  const podcamul = document.querySelector('podcamul');
+  const podca = document.querySelector('.podca');
   init(podcamul,podca);
 };
 
